d632a7d89f2dbd373bbb29b6a9ae690581ce2a58
koyu <me@koyu.space>
2021-10-13 00:08:45 +0200

    Added proper error/success messages

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -1,6 +1,26 @@
 <template>
   <div>
+    <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+      <symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16">
+        <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+      </symbol>
+      <symbol id="check-circle-fill" fill="currentColor" viewBox="0 0 16 16">
+        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+      </symbol>
+    </svg>
     <div class="container" style="padding: 50px 0 100px 0">
+      <div class="alert alert-danger d-flex align-items-center" role="alert" v-bind:data-error="isError">
+        <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill"/></svg>
+        <div>
+          {{ errorMessage }}
+        </div>
+      </div>
+      <div class="alert alert-success d-flex align-items-center" role="alert" v-bind:data-success="isSuccess">
+        <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
+        <div>
+          Check your email for the login link!
+        </div>
+      </div>
       <form class="row flex flex-center" @submit.prevent="handleLogin">
         <div class="col-6 form-widget">
           <h1 class="header logo">김치</h1>
@@ -37,16 +57,22 @@ import { supabase } from "../supabase"
 export default {
   setup() {
     const loading = ref(false)
+    const isError = ref(false)
+    const isSuccess = ref(false)
     const email = ref("")
+    const errorMessage = ref("")
 
     const handleLogin = async () => {
+      isError.value = false
+      isSuccess.value = false
       try {
         loading.value = true
         const { error } = await supabase.auth.signIn({ email: email.value })
         if (error) throw error
-        alert("Check your email for the login link!")
+        isSuccess.value = true
       } catch (error) {
-        alert(error.error_description || error.message)
+        errorMessage.value = error.error_description || error.message;
+        isError.value = true
       } finally {
         loading.value = false
       }
@@ -56,6 +82,9 @@ export default {
       loading,
       email,
       handleLogin,
+      isError,
+      errorMessage,
+      isSuccess,
     }
   },
 }
@@ -83,5 +112,11 @@ footer {
 }
 .btn {
   margin-top: 20px;
+}
+div[data-error="false"] {
+  display: none !important;
+}
+div[data-success="false"] {
+  display: none !important;
 }
 </style>
--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,5 +1,25 @@
 <template>
   <div>
+    <svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+      <symbol id="exclamation-triangle-fill" fill="currentColor" viewBox="0 0 16 16">
+        <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
+      </symbol>
+      <symbol id="check-circle-fill" fill="currentColor" viewBox="0 0 16 16">
+        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+      </symbol>
+    </svg>
+    <div class="alert alert-danger d-flex align-items-center" role="alert" v-bind:data-error="isError">
+      <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill"/></svg>
+      <div>
+        {{ errorMessage }}
+      </div>
+    </div>
+    <div class="alert alert-success d-flex align-items-center" role="alert" v-bind:data-success="isSuccess">
+      <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
+      <div>
+        Profile successfully updated!
+      </div>
+    </div>
     <h1>Edit profile</h1>
     <form class="form-widget" @submit.prevent="updateProfile">
       <div id="profile">
@@ -45,10 +65,14 @@ export default {
     const username = ref("")
     const website = ref("")
     const avatar_url = ref("")
+    const isError = ref(false)
+    const errorMessage = ref("")
+    const isSuccess = ref("false")
 
     async function getProfile() {
       try {
         loading.value = true
+        isError.value = false
         store.user = supabase.auth.user()
 
         let { data, error, status } = await supabase
@@ -68,7 +92,8 @@ export default {
           store.user.greeting = user.email
         }
       } catch (error) {
-        alert(error.message)
+        errorMessage.value = error.message
+        isError.value = true
       } finally {
         loading.value = false
       }
@@ -77,6 +102,8 @@ export default {
     async function updateProfile() {
       try {
         loading.value = true
+        isError.value = false
+        isSuccess.value = false
         store.user = supabase.auth.user()
 
         const updates = {
@@ -91,9 +118,14 @@ export default {
           returning: "minimal", // Don't return the value after inserting
         })
 
-        if (error) throw error
+        if (error) {
+          throw error
+        } else {
+          isSuccess.value = true
+        }
       } catch (error) {
-        alert(error.message)
+        errorMessage.value = error.message
+        isError.value = true
       } finally {
         loading.value = false
       }
@@ -109,8 +141,10 @@ export default {
       username,
       website,
       avatar_url,
-
-      updateProfile
+      updateProfile,
+      isError,
+      errorMessage,
+      isSuccess,
     }
   },
 }
@@ -125,8 +159,22 @@ export default {
 .btn {
   margin-top: 20px;
 }
-h1 {
+div {
   text-align: center;
+}
+.alert {
+  width: 50%;
+  margin: 0 auto;
+  margin-bottom: 20px;
   margin-top: 20px;
+}
+h1 {
+  margin-top: 20px;
+}
+div[data-error="false"] {
+  display: none !important;
+}
+div[data-success="false"] {
+  display: none !important;
 }
 </style>
